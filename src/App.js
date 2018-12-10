9095eb9f52ac6eca44be23236408f686a03dc283
Frank J. Reashore <frank.reashore@bellin.com>
2018-12-10 09:50:53 -0800

    Added react-table code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,69 @@
 import React, { Component } from 'react';
+import ReactTable from 'react-table';
 import './App.css';
+import 'react-table/react-table.css';
 
 class App extends Component {
+  // render() {
+  //   return (
+  //     <div className="App">React Table Demo</div>
+  //   );
+  // }
+
   render() {
-    return (
-      <div className="App">React Table Demo</div>
-    );
-  }
+    const data = [
+      {
+        name: 'Person1',
+        age: 26,
+        friend: {
+          name: 'Friend1',
+          age: 23
+        }
+      },
+      {
+        name: 'Person2',
+        age: 26,
+        friend: {
+          name: 'Friend2',
+          age: 23
+        }
+      },
+      {
+        name: 'Person3',
+        age: 26,
+        friend: {
+          name: 'Friend3',
+          age: 23
+        }
+      }
+    ];
+  
+    const columns = [
+      {
+        Header: 'Name',
+        accessor: 'name' // String-based value accessors!
+      }, 
+      {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+      }, 
+      {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name // Custom value accessors!
+      }, 
+      {
+        Header: props => <span>Friend Age</span>, // Custom header components!
+        accessor: 'friend.age'
+      }
+  ];
+  
+    <ReactTable
+      data={data}
+      columns={columns}
+    />
+  }  
 }
 
 export default App;
